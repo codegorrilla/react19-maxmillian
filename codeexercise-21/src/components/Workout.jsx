a07d7f0761cf19afc82a4af23300e0d518123c25
Sanjib Adhya <code.gorrilla@gmail.com>
2025-05-19 10:46:16 +0530

    added exercise related to refs and portals

---
+++ codeexercise-21/src/components/Workout.jsx
@@ -0,0 +1,47 @@
+import { useState, useRef } from 'react';
+
+export default function Workout({ title, description, time, onComplete }) {
+	const timer = useRef();
+	const [timerRunning, setTimerRunning] = useState(false);
+	const [timerStopped, setTimerStopped] = useState(false);
+
+	function handleStartWorkout() {
+		// Todo: Start timer
+		timer.current = setTimeout(() => {
+			onComplete();
+			setTimerStopped(true);
+		}, time);
+
+		//console.log(`Timer started: ${time / 1000} seconds`);
+
+		setTimerRunning(true);
+	}
+
+	function handleStopWorkout() {
+		// Todo: Stop timer
+		clearTimeout(timer.current);
+		onComplete();
+	}
+
+	return (
+		<article className='workout'>
+			<h3>{title}</h3>
+			<p>{description}</p>
+			<p>{time}</p>
+			<p>
+				<button
+					className={timerRunning ? 'active' : undefined}
+					onClick={handleStartWorkout}
+				>
+					Start
+				</button>
+				<button
+					className={timerStopped ? 'active' : undefined}
+					onClick={handleStopWorkout}
+				>
+					Stop
+				</button>
+			</p>
+		</article>
+	);
+}
